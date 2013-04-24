81d7471efc63e7c1bb24f16f69e1b3d8ab8bde2f
josephodada@gmail.com <josephodada@gmail.com@5d793669-5241-24e6-4ac0-c47ef1b607d1>
2013-04-24 16:03:01 +0000

    Initial LIBNUML API implementation.

--- trunk/libnuml/src/numl/Result.cpp
+++ trunk/libnuml/src/numl/Result.cpp
@@ -1,10 +1,10 @@
 /**
 * Begin svn Header
-* $Rev::			$:	Revision of last commit
-* $Author::			$:	Author of last commit
-* $Date::			$:	Date of last commit
-* $HeadURL::		$
-* $Id::				$
+* $Rev$:	Revision of last commit
+* $Author$:	Author of last commit
+* $Date$:	Date of last commit
+* $HeadURL$
+* $Id$
 * End svn Header
 * ****************************************************************************
 * This file is part of libNUML.  Please visit http://code.google.com/p/numl/for more
