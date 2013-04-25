72dcf86aa93f4ab7bb9db23aa7b6b1048920522a
Joseph O Dada <joseph.dada@manchester.ac.uk>
2013-04-25 12:20:52 +0000

    Initial LIBNUML API implementation.

--- libnuml/src/numl/SyntaxChecker.h
+++ libnuml/src/numl/SyntaxChecker.h
@@ -1,26 +1,50 @@
 /**
- * @file    SyntaxChecker.h
+ * @file    SyntaxChecker.cpp
  * @brief   Syntax checking functions
- * @author  Sarah Keating
- *
- * $Id$
- * $HeadURL$
- *
- *<!---------------------------------------------------------------------------
- * This file is part of libNUML.  Please visit http://numl.org for more
- * information about NUML, and the latest version of libNUML.
- *
- * Copyright 2005-2009 California Institute of Technology.
- * Copyright 2002-2005 California Institute of Technology and
- *                     Japan Science and Technology Corporation.
- * 
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is provided
- * in the file named "LICENSE.txt" included with this software distribution
- * and also available online as http://numl.org/software/libnuml/license.html
- *----------------------------------------------------------------------- -->
- *
+* Begin svn Header
+* $Rev$:	Revision of last commit
+* $Author$:	Author of last commit
+* $Date$:	Date of last commit
+* $HeadURL$
+* $Id$
+* End svn Header
+* ****************************************************************************
+* This file is part of libNUML.  Please visit http://code.google.com/p/numl/for more
+* information about NUML, and the latest version of libNUML.
+* Copyright (c) 2013 The University of Manchester.
+*
+* This library is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation.  A copy of the license agreement is
+* provided in the file named "LICENSE.txt" included with this software
+* distribution and also available online as http://www.gnu.org/licenses/lgpl.html
+*
+* The original code was initially developed by:
+*
+* 	SBML Team
+* 	Control and Dynamical Systems, MC 107-81
+* 	California Institute of Technology
+* 	Pasadena, CA, 91125, USA
+*
+*  	http://www.sbml.org
+* 	mailto:sbml-team@caltech.edu
+*
+* Adapted for NUML by:
+*
+* 	NUML Team
+* 	Computational Systems Biology Group
+* 	School of Computer Science
+* 	The University of Manchester
+* 	Manchester, M1 7DN UK
+* 	mailto:josephodada@gmail.com
+*
+*   Contributor(s):
+*   Sarah Keating
+*   Joseph O. Dada, The University of Manchester - initial LIBNUML API and implementation
+* ****************************************************************************
+**/
+
+/*
  * @class SyntaxChecker
  * @brief Methods for checking syntax of ids, metaids and units
  *
@@ -112,7 +136,7 @@ public:
    *
    * @return @c true if the XMLNode conforms, @c false otherwise.
    *
-   * @note the optional NUMLNamespaces argument can be used to
+   * @note the optional NUMLNamespaces artype filter textgument can be used to
    * check for the declaration of the XHTML namespace at the top-level
    * within an NUMLDocument.
    */
