7daabd66350ff9382b92aafc100cfeef011f43ef
Joseph O Dada <joseph.dada@manchester.ac.uk>
2013-04-25 09:07:15 +0000

    Initial LIBNUML API implementation.

--- libnuml/src/xml/LibXMLHandler.cpp
+++ libnuml/src/xml/LibXMLHandler.cpp
@@ -1,25 +1,48 @@
 /**
  * @file    LibXMLHandler.cpp
  * @brief   Redirect LibXML events to an XMLHandler
- * @author  Ben Bornstein
- *
- * $Id$
- * $HeadURL$
- *
- *<!---------------------------------------------------------------------------
- * This file is part of libNUML.  Please visit http://numl.org for more
- * information about NUML, and the latest version of libNUML.
- *
- * Copyright 2005-2009 California Institute of Technology.
- * Copyright 2002-2005 California Institute of Technology and
- *                     Japan Science and Technology Corporation.
- * 
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is provided
- * in the file named "LICENSE.txt" included with this software distribution and
- * also available online as http://numl.org/software/libnuml/license.html
- *----------------------------------------------------------------------- -->*/
+* Begin svn Header
+* $Rev$:	Revision of last commit
+* $Author$:	Author of last commit
+* $Date$:	Date of last commit
+* $HeadURL$
+* $Id$
+* End svn Header
+* ****************************************************************************
+* This file is part of libNUML.  Please visit http://code.google.com/p/numl/for more
+* information about NUML, and the latest version of libNUML.
+* Copyright (c) 2013 The University of Manchester.
+*
+* This library is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation.  A copy of the license agreement is
+* provided in the file named "LICENSE.txt" included with this software
+* distribution and also available online as http://www.gnu.org/licenses/lgpl.html
+*
+* The original code was initially developed by:
+*
+* 	SBML Team
+* 	Control and Dynamical Systems, MC 107-81
+* 	California Institute of Technology
+* 	Pasadena, CA, 91125, USA
+*
+*  	http://www.sbml.org
+* 	mailto:sbml-team@caltech.edu
+*
+* Adapted for NUML by:
+*
+* 	NUML Team
+* 	Computational Systems Biology Group
+* 	School of Computer Science
+* 	The University of Manchester
+* 	Manchester, M1 7DN UK
+* 	mailto:josephodada@gmail.com
+*
+*   Contributor(s):
+*   Ben Bornstein
+*   Joseph O. Dada, The University of Manchester - initial LIBNUML API and implementation
+* ****************************************************************************
+**/
 
 #include <numl/xml/XMLHandler.h>
 #include <numl/xml/XMLTriple.h>
