35165a731302109baefb3f24c31b8c6ec39da2af
Joseph O Dada <joseph.dada@manchester.ac.uk>
2013-06-26 11:01:53 +0000

    Change the build tool from Autotools to CMake. Create the CMake files and delete all the Autotool files. Remove the XML parser layer and make LibNUML depend on LibSBML to avoid conflicts in using the two libraries together.

--- libnuml/examples/java/CMakeFiles/example_java_printNUML.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/example_java_printNUML"
-  "printNUML.class"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/example_java_printNUML.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
